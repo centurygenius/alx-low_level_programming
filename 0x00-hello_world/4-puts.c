0a451c0862a3f9e2ad6c3036c408e9eef5c33fa2
samuel abiona <samuelo.abiona@gmail.com>
2023-07-13 17:53:37 -0700

    C program that prints

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+/**
+* A C-program that prints a line of text
+* The return value is 0
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (o);
+}
