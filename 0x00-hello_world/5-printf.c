6660e221e225ae2f064b8a14df5cebfa2131e408
samuel abiona <samuelo.abiona@gmail.com>
2023-07-13 19:32:30 -0700

    C program that prints

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+/**
+*main - Entry point
+*Description:This program prints with printf function and return 0
+*Return: 0(successful)
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
