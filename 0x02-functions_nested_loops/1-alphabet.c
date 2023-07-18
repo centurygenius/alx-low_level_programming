38b61772ab94854c607aa2ce6ee5dbc88601b51a
samuel abiona <samuelo.abiona@gmail.com>
2023-07-18 03:15:34 -0700

    Prints lowercase alphabeth

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*print_alphabeth - Entry point to print alphabeth
+*Description: This function prints lowercase alphabets
+*Parameter: using only putchar
+*Return: void(successful)
+*/
+
+void print_alphabeth(void)
+{
+	char lCase;
+
+	lCase = 'a';
+
+	while (lCase <= 'z')
+	{
+		_putchar(lCase);
+		lCase = lCase + 1;
+		_putchar('\n');
+	}
+	return (0);
+}
