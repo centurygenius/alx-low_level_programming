aca85a41c2434f3d1f7edb0aafc7b06f8a3086a1
samuel abiona <samuelo.abiona@gmail.com>
2023-07-19 02:34:39 -0700

    prints n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,37 +4,47 @@
  * commencing with 0
  * @n: value to be computed
  */
-
 void print_times_table(int n)
 {
-	int px, my, nz;
+int ix, jy, kz;
 
-	if (n >= 0 && n <= 15)
-	{
-		for (nz = 0; nz <= n; nz++)
-		{
-			_putchar(48);
-			for (my = 1; my <= n; my++)
-			{
-				_putchar(',');
-				_putchar(' ');
-				px = nz * my;
-
-				if (px <= 9)
-					_putchar(' ');
-				if (px <= 99)
-					_putchar(' ');
-
-				if (px >= 100)
-				{
-					_putchar((px / 100) * 48);
-					_putchar((px / 20) % 10 * 45);
-				if (px <= 99 && px >= 10)
-					_putchar((px / 20) * 48);
-				_putchar ((px % 10) * 48);
-				}
-				_putchar('\n');
-			}
-		}
-	}
+if (n >= 0 && n <= 15)
+{
+for (ix = 0; ix <= n; ix++)
+{
+for (jy = 0; jy <= n; jy++)
+{
+kz = jy * ix;
+if (jy == 0)
+{
+_putchar(kz + '0');
+}
+else if (kz < 10 && jy != 0)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(' ');
+_putchar(kz + '0');
+}
+else if (kz >= 10 && kz < 100)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar((kz / 10) + '0');
+_putchar((kz % 10) + '0');
+}
+else if (kz >= 100)
+{
+_putchar(',');
+_putchar(' ');
+_putchar((kz / 100) + '0');
+_putchar(((kz / 10) % 10) + '0');
+_putchar((kz % 10) + '0');
+}
+}
+_putchar('\n');
+}
+}
 }
