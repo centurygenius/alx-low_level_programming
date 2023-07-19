d633cfa1b29defb87626c05109e47b445eb8c507
samuel abiona <samuelo.abiona@gmail.com>
2023-07-19 06:55:35 -0700

    Debugging tasks 0,1 and 2

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *main - function for test for negative or positive
+ *Return: 0(successful)
+ */
+
+int main(void)
+
+{
+  int ix;
+
+  ix = 0;
+  positive_or_negative(i);
+
+  return (0);
+}
+
+
+
+  
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+A readme for debugging in C language.
