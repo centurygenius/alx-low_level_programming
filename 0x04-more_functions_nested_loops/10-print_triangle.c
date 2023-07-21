eec1b30ae9ddab919bfaf19b371f5ca6ab5ac15a
samuel abiona <samuelo.abiona@gmail.com>
2023-07-20 21:48:30 -0700

    Printd diagonal triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,4 +1,33 @@
 #include "main.h"
 
 /**
- *
+ * print_triangle - Entry point
+ * Description: prints diagonal
+ * @size: size of the triangle to be printed
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int row, hashes, spaces;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (row = 1; row <= size; row = row + 1)
+		{
+			for (spaces = size - row; spaces >= 1; spaces = spaces + 1)
+			{
+				_putchar(' ');
+			}
+			for (hashes = 1; hashes <= row; hashes = hashes + 1)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
