a5f5853b2a75632b93d4eaa72ed986ee5fba2330
samuel abiona <samuelo.abiona@gmail.com>
2023-08-15 04:30:39 -0700

    function that initialize a variable of type struct dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,6 +9,7 @@
  * @age: age to initialize
  * @owner: owner to initialize
  */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	d = malloc(sizeof(struct dog));
@@ -17,5 +18,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
-	return (d);
 }
