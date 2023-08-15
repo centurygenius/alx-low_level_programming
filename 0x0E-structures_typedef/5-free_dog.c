c748fc51dbd884bbea3c95cc0757e001745f68e2
samuel abiona <samuelo.abiona@gmail.com>
2023-08-15 05:48:30 -0700

    function that frees dogs.

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees memory allocated for a struct dog
+ * @d: struct dog to free
+ */
+
+void free_dog(dog_t *d)
+{
+	d = malloc(sizeof(struct dog_t);
+	if (d == NULL)
+		return (NULL);
+	else
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
