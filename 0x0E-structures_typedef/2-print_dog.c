86b62ee4dee9bbe41834c87a65b7a4c7df39aaf7
samuel abiona <samuelo.abiona@gmail.com>
2023-08-15 05:06:05 -0700

    function that prints a struct dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,8 +13,8 @@ void print_dog(struct dog *d)
 		return;
 
 	if ((*d).name == NULL)
-		(*d).name = " ";
+		(*d).name = "(nil)";
 	if ((*d).owner == NULL)
-		(*d).owner = " ";
+		(*d).owner = "(nil)";
 	printf("Name: %s\nAge: %f\nOwner: %s\n", (*d).name, (*d).age, (*d).owner);
 }
