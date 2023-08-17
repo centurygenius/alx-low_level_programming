5682b6451aeb648c4d4747079807f4315866894d
samuel abiona <samuelo.abiona@gmail.com>
2023-08-17 04:35:31 -0700

    Sums all parameters

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - Entry point
+ * Description: sums all of its parameters
+ * @n: total number of parameters
+ * @...: ellipsis represents variable number of parameters
+ * Return: 0 if n == 0
+ *         otherwise the sum of all the parameters
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list vlists;
+
+	va_start(vlists, n);
+
+	unsigned int i, sum = 0;
+
+	i = 0;
+	while (i < n)
+	{
+		if (n == 0)
+			return (0);
+		sum += va_arg(vlists, int);
+		i++;
+	}
+	va_end(vlists);
+	return (sum);
+}
