e6151c18cd4ba9cc0c1dbaac3f127c394d307979
samuel abiona <samuelo.abiona@gmail.com>
2023-08-17 04:57:27 -0700

    sums all the parameters

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -18,12 +18,9 @@ int sum_them_all(const unsigned int n, ...)
 
 	unsigned int i, sum = 0;
 
-	i = 0;
-	while (i < n)
-	{
+	for (i = 0; i < n; i++)
 		sum += va_arg(vlists, int);
-		i++;
-	}
+
 	va_end(vlists);
 	return (sum);
 }
