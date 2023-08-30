ba402a2987584ec3e379f5b5422c975aa79d7942
samuel abiona <samuelo.abiona@gmail.com>
2023-08-30 07:46:56 -0700

    Frees a linked list

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - Entry point
+ * Description: frees a linked list
+ * @h: pointer to the first node in the linked list
+ * Return: number of elements in the freed list
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
+	size_t lenght = 0;
+	int diff;
+	listint_t *temp;
+
+	if (!*h)
+		return (0);
+	if (!h)
+		return (0);
+
+	while (*h != NULL)
+	{
+		diff = *h - (**h).next;
+		if (diff > 0)
+		{
+			temp = (**h).next;
+			free(*h);
+			*h = temp;
+			lenght++;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			lenght++;
+			break;
+		}
+	}
+
+	*h = NULL;
+
+	return (lenght);
+}
