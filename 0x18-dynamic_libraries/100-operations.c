23e05f3bc55596aa0ca5473bfeb3f4d7ac4131dd
samuel abiona <samuelo.abiona@gmail.com>
2024-01-16 08:36:49 -0800

    second commit

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/**
+ * add - sums two integers
+ *@a: first num
+ *@b: second num
+ *Return: addition
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts two integers
+ *@a: first num
+ *@b: second num
+ *Return: differece
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies two integers
+ *@a: first num
+ *@b: second num
+ *Return: multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - division of two integers
+ *@a: first num
+ *@b: second num
+ *Return: division
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - modulus of two integers
+ *@a: first num
+ *@b: second num
+ *Return: modulus
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
--- 0x18-dynamic_libraries/3-puts.c
+++ 0x18-dynamic_libraries/3-puts.c
@@ -3,12 +3,13 @@
  * _puts - prints a str, followed by a new line,
  * @str: pointer to the string to print
  * Return: void
-*/
+ */
 
 
 void _puts(char *str)
 {
 	int index = 0;
+
 	while (str[index])
 	{
 		_putchar(str[index]);
